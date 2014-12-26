8608119bcf492a17d3831168e12e10348d7c26b0
amen souissi <amensouissi@ecreall.com>
2014-12-26 17:21:27 +0100

    Remove unused mails

--- novaideo/mail.py
+++ novaideo/mail.py
@@ -182,43 +182,6 @@ Bonjour {recipient_title} {recipient_last_name},
 """+ PORTAL_SIGNATURE
 
 
-ARCHIVEIDEA_SUBJECT = u"""Décision des modérateur sur l'idée {subject_title}"""
-
-
-ARCHIVEIDEA_MESSAGE = u"""
-Bonjour {recipient_title} {recipient_last_name},
-
-L'idée {subject_title} viens d'être archivée par les modérateurs pour la raison suivante: 
-
-{explanation}
-
-"""+ PORTAL_SIGNATURE
-
-
-ALERTOPINION_SUBJECT = u"""Avis des modérateur sur la proposition {subject_title}"""
-
-
-ALERTOPINION_MESSAGE = u"""
-Bonjour {recipient_title} {recipient_last_name},
-
-les modérateurs ont émis un avis {opinion} sur la proposition {subject_title}: 
-
-{explanation}
-
-"""+ PORTAL_SIGNATURE
-
-
-PUBLISHEDIDEA_SUBJECT = u"""Décision des modérateur sur l'idée {subject_title}"""
-
-
-PUBLISHEDIDEA_MESSAGE = u"""
-Bonjour {recipient_title} {recipient_last_name},
-
-L'idée {subject_title} viens d'être publiée par les modérateurs.
-
-"""+ PORTAL_SIGNATURE
-
-
 PROPOSALREMOVED_SUBJECT = u"""Suppression de la proposition {subject_title}"""
 
 
